8ec3a3de4fdfed5c14b81efd663d7e59253bc0f3
raja <yallalavenkatarajareddy@gmail.com>
2023-06-08 09:43:44 +0530

    Check authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,18 @@
 const Post = require('../models/post');
 module.exports.home = function(req,res){
 
-    console.log(req.cookies);
-    //Altering the cookie
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title:'Home'
+    Post.find({},function(err,posts){
+        return res.render('home',{
+            title:"Home",
+            posts:posts
+        });
     });
+
+    //populate user of each post 
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+        });
+    })
 }
